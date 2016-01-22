3d20cae857553c81e5bb1e9b4edc50c1043b2c94
Josh Sephton <josh@383project.com>
2016-01-22 13:44:09 +0000

    feat: auth journey and publish a new post

--- index.js
+++ index.js
@@ -1,26 +1,79 @@
+var express = require('express');
 var medium = require('medium-sdk')
 
-var client = new medium.MediumClient({
-	clientId: 'YOUR_CLIENT_ID',
-	clientSecret: 'YOUR_CLIENT_SECRET'
-})
+var app = express();
 
-var url = client.getAuthorizationUrl('secretState', 'https://yoursite.com/callback/medium', [
-  medium.Scope.BASIC_PROFILE, medium.Scope.PUBLISH_POST
-])
+app.get('/auth', function (req, res) {
+	var client = new medium.MediumClient({
+		clientId: '4d200404ffd3',
+		clientSecret: '47a9b71ce0c333024065a2452d9dc20562db5f9d'
+	});
+	var url = client.getAuthorizationUrl(
+		'secretState',
+		'http://127.0.0.1:5000/auth/callback',
+		[medium.Scope.BASIC_PROFILE, medium.Scope.PUBLISH_POST]
+	);
+	res.redirect(url);
+});
 
-// (Send the user to the authorization URL to obtain an authorization code.)
+app.get('/auth/callback', function (req, res) {
+	var client = new medium.MediumClient({
+		clientId: '4d200404ffd3',
+		clientSecret: '47a9b71ce0c333024065a2452d9dc20562db5f9d'
+	});
+	client.exchangeAuthorizationCode(
+		req.query.code,
+		'http://127.0.0.1:5000/auth/callback',
+		function (err, token) {
+			client.getUser(function (err, user) {
+				// client.createPost({
+				// 	userId: user.id,
+				// 	title: 'A new post',
+				// 	contentFormat: medium.PostContentFormat.HTML,
+				// 	content: '<h1>A New Post</h1><p>This is my new post.</p>',
+				// 	publishStatus: medium.PostPublishStatus.PUBLIC,
+				// 	publishedAt: '2004-02-12T15:19:21+00:00'
+				// }, function (err, post) {
+				// 	console.log(token, user, post);
+				// });
+			});
+		}
+	);
+});
 
-client.ExchangeAuthorizationCode('YOUR_AUTHORIZATION_CODE', function (err, token) {
-	client.getUser(function (err, user) {
-		client.createPost({
-			userId: user.id,
-			title: 'A new post',
-			contentFormat: medium.PostContentFormat.HTML,
-			content: '<h1>A New Post</h1><p>This is my new post.</p>',
-			publishStatus: medium.PostPublishStatus.DRAFT
-		}, function (err, post) {
-			console.log(token, user, post)
-		})
-	})
-})
+
+app.listen(5000, function () {
+ 	console.log('Example app listening on port 5000!');
+});
+
+//
+// var medium = require('medium-sdk')
+//
+// var client = new medium.MediumClient({
+// 	clientId: '4d200404ffd3',
+// 	clientSecret: '47a9b71ce0c333024065a2452d9dc20562db5f9d'
+// });
+//
+// var url = client.getAuthorizationUrl(
+// 	'secretState',
+// 	'http://127.0.0.1:5000/callback',
+// 	[medium.Scope.BASIC_PROFILE, medium.Scope.PUBLISH_POST]
+// );
+//
+// client.exchangeAuthorizationCode(code, redirectUrl, callback) {
+//
+// // (Send the user to the authorization URL to obtain an authorization code.)
+//
+// client.ExchangeAuthorizationCode('YOUR_AUTHORIZATION_CODE', function (err, token) {
+// 	client.getUser(function (err, user) {
+// 		client.createPost({
+// 			userId: user.id,
+// 			title: 'A new post',
+// 			contentFormat: medium.PostContentFormat.HTML,
+// 			content: '<h1>A New Post</h1><p>This is my new post.</p>',
+// 			publishStatus: medium.PostPublishStatus.DRAFT
+// 		}, function (err, post) {
+// 			console.log(token, user, post)
+// 		});
+// 	});
+// });
